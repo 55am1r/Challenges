628ea0586ce26db94b0d4a689f66945ce1cca378
Shaik Sameer <shaiksameer515595@gmail.com>
2022-12-06 17:41:32 +0530

    saving new changes in piano challenge

---
+++ piano_keyboard/script.js
@@ -0,0 +1,51 @@
+document.getElementById("main-keys-1").addEventListener("mouseover", () => {
+  new Audio("src/key01.mp3").play();
+});
+document.getElementById("main-keys-2").addEventListener("mouseover", () => {
+  new Audio("src/key02.mp3").play();
+});
+document.getElementById("main-keys-3").addEventListener("mouseover", () => {
+  new Audio("src/key03.mp3").play();
+});
+document.getElementById("main-keys-4").addEventListener("mouseover", () => {
+  new Audio("src/key04.mp3").play();
+});
+document.getElementById("main-keys-5").addEventListener("mouseover", () => {
+  new Audio("src/key05.mp3").play();
+});
+document.getElementById("main-keys-6").addEventListener("mouseover", () => {
+  new Audio("src/key06.mp3").play();
+});
+document.getElementById("main-keys-7").addEventListener("mouseover", () => {
+  new Audio("src/key07.mp3").play();
+});
+document.getElementById("main-keys-8").addEventListener("mouseover", () => {
+  new Audio("src/key08.mp3").play();
+});
+document.getElementById("main-keys-9").addEventListener("mouseover", () => {
+  new Audio("src/key09.mp3").play();
+});
+document.getElementById("main-keys-10").addEventListener("mouseover", () => {
+  new Audio("src/key10.mp3").play();
+});
+document.getElementById("inner-key-1").addEventListener("mouseover", () => {
+  new Audio("src/key11.mp3").play();
+});
+document.getElementById("inner-key-2").addEventListener("mouseover", () => {
+  new Audio("src/key12.mp3").play();
+});
+document.getElementById("inner-key-3").addEventListener("mouseover", () => {
+  new Audio("src/key13.mp3").play();
+});
+document.getElementById("inner-key-4").addEventListener("mouseover", () => {
+  new Audio("src/key14.mp3").play();
+});
+document.getElementById("inner-key-5").addEventListener("mouseover", () => {
+  new Audio("src/key15.mp3").play();
+});
+document.getElementById("inner-key-6").addEventListener("mouseover", () => {
+  new Audio("src/key16.mp3").play();
+});
+document.getElementById("inner-key-7").addEventListener("mouseover", () => {
+  new Audio("src/key17.mp3").play();
+});
